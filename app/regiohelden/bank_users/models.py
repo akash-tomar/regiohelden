e0067e65913ac0144ac9aff079e021c35a55b3cc
Akash Tomar <akash.tomar217@gmail.com>
2017-06-10 17:25:13 +0530

    db schema created

--- app/regiohelden/bank_users/models.py
+++ app/regiohelden/bank_users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class IBANDetail(models.Model):
+	countrycode = models.CharField(max_length=2)
+	checksum = models.CharField(max_length=2)
+	swiftcode = models.CharField(max_length=4)
+	accountnumber = models.CharField(max_length=24)
+	def __str__(self):
+		return str(self.bank_user.user.email)
+
+class BankUser(models.Model):
+	user = models.OneToOneField(User,related_name="bank_user")	
+	iban = models.OneToOneField(IBANDetail,related_name="bank_user")
+	def __str__(self):
+		return str(self.user.email)
