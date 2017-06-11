5215c6bbc43292a71c3b74c89a601d8c70ed73a3
Akash Tomar <akash.tomar217@gmail.com>
2017-06-12 01:11:28 +0530

    Added comments and refactored code. Template inheritance. Bug-User updation validation not present

--- app/regiohelden/bank_users/forms.py
+++ app/regiohelden/bank_users/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import *
 from django.contrib.auth.models import User
 import re
+
 class UserForm(forms.ModelForm):
 	class Meta:
 		model = CustomUser
@@ -12,11 +13,13 @@ class UserForm(forms.ModelForm):
 		self.fields['first_name'].widget.attrs.update({'placeholder':'First name'})
 		self.fields['last_name'].widget.attrs.update({'placeholder':'Last name'})
 
+	#Raises validation error if string is not alphabet.
 	def clean_first_name(self):
 		if not re.match(r'^[a-zA-Z]+$', self.cleaned_data.get('first_name')):
 			raise TypeError("Only alphabets allowed in names")
 		return self.cleaned_data.get('first_name')
 
+	#Raises validation error if string is not alphabet.
 	def clean_last_name(self):
 		if not re.match(r'^[a-zA-Z]+$', self.cleaned_data.get('last_name')):
 			raise TypeError("Only alphabets allowed in names")
@@ -33,23 +36,27 @@ class IBANForm(forms.ModelForm):
 		self.fields['checksum'].widget.attrs.update({'placeholder':'Check sum'})
 		self.fields['swiftcode'].widget.attrs.update({'placeholder':'Swift code'})
 		self.fields['accountnumber'].widget.attrs.update({'placeholder':'Account Number'})
-
+	
+	#Raises validation error if string is not alphabet.
 	def clean_countrycode(self):
 		countrycode = self.cleaned_data.get('countrycode')
 		if not countrycode.isalpha() or len(countrycode)!=2:
 			raise TypeError("Invalid country code")
 		return self.cleaned_data.get('countrycode')
 
+	#Raises validation error if name is not numerical.
 	def clean_checksum(self):
 		if not self.cleaned_data.get('checksum').isdigit():
 			raise TypeError("Checksum can only be numeric")
 		return self.cleaned_data.get('checksum')
 
+	#Raises validation error if name is not numerical.
 	def clean_accountnumber(self):
 		if not self.cleaned_data.get('accountnumber').isdigit():
 			raise TypeError("Account number can only be numeric")
 		return self.cleaned_data.get('accountnumber')	
 
+	#Raises validation error if name is not alphanumerical.
 	def clean_swiftcode(self):
 		if not self.cleaned_data.get('swiftcode').isalnum():
 			raise TypeError("Swift code can only be alpha numeric")
--- app/regiohelden/bank_users/views.py
+++ app/regiohelden/bank_users/views.py
@@ -8,19 +8,26 @@ from django.contrib.auth.models import User
 from django.http import JsonResponse
 import json
 
+
+#if the user is already logged, redirects to home page.
 def login(request):
 	if request.user.is_anonymous():
 		return render(request,'login.html',{})
 	else:
 		return HttpResponseRedirect(reverse('bank_users:home'))
 
+#redirects to login page after logging out the user.
 def logout(request):
 	logout_user(request)
 	return HttpResponseRedirect(reverse('bank_users:login'))
 
+#All the operations are listed on this page.
 def home(request):
 	if request.user.is_anonymous():
 		return HttpResponseRedirect(reverse('bank_users:login'))
+
+	'''These variables are flags that keep note of data being created or modified or 
+	deleted so that user can be notified accordingly.'''
 	created = False
 	updated = False
 	deleted = False
@@ -37,10 +44,12 @@ def home(request):
 	if 'delete' in request.GET:
 		if request.GET.get('delete')=="1":
 			deleted=True
-	return render(request,'home.html',{'created':created,'updated':updated,'deleted':deleted})
+	return render(request,'home.html',{'created':created,'updated':updated,'deleted':deleted })
 
 def createBankingUser(request):
 	if request.method=='GET':
+
+		#flag for if the user tried but failed on creation of bank user.
 		created = True
 		if 'create' in request.GET:
 			created = request.GET.get('create')
@@ -56,9 +65,7 @@ def createBankingUser(request):
 		ibanform = IBANForm(request.POST)
 		userform = UserForm(request.POST)
 
-		# check whether it's valid:
-		# import pdb; pdb.set_trace()
-		# userform_ = userform.clean_names()
+		#check the validity of forms
 		if userform.is_valid() and ibanform.is_valid():
 			first_name = userform.cleaned_data.get('first_name')
 			last_name = userform.cleaned_data.get('last_name')
@@ -74,6 +81,7 @@ def createBankingUser(request):
 				user = CustomUser(first_name=first_name,last_name=last_name)
 				user.save()
 			except:
+				#User was not found with this combination of first_name and last_name
 				return HttpResponseRedirect(reverse('bank_users:addUser')+'?create=0')
 
 			iban = IBANDetail(countrycode=countrycode,checksum=checksum,swiftcode=swiftcode,accountnumber=accountnumber)
@@ -81,22 +89,26 @@ def createBankingUser(request):
 			creator = User.objects.get(pk=request.user.id)
 			bankuser = BankUser(user=user,iban=iban,creator=creator)
 			bankuser.save()
-
 			return HttpResponseRedirect(reverse('bank_users:home')+'?create=1')
 		else:
+			#invalid form
 			return HttpResponseRedirect(reverse('bank_users:addUser')+'?create=0')
 
-
+#function to display the details of bank user.
 def readBankingUser(request):
 	if request.is_ajax():
 		first_name = request.GET.get('first_name')
 		last_name = request.GET.get('last_name')
 		obj = {'first_name':first_name,'last_name':last_name}
+
 		user = None
 		try:
 			user = CustomUser.objects.get(first_name__icontains=first_name,last_name__icontains=last_name)
 		except:
+			#user not found with this combination of first and last names.
 			return JsonResponse({"failed":"Wrong combination of first name and last name"})
+
+		#get user details and send them to the template.
 		obj['countrycode']=user.bank_user.iban.countrycode
 		obj['checksum']=user.bank_user.iban.checksum
 		obj['swiftcode']=user.bank_user.iban.swiftcode
@@ -108,21 +120,26 @@ def readBankingUser(request):
 
 def updateBankingUser(request):
 	if request.method=="POST":
+
+		#In python3 byte cannot be converted to json directly therefore it needs to be decoded.
 		body_unicode = request.body.decode('utf-8')
 		data = json.loads(body_unicode)
 
 		first_name = data.get('old_first_name')
 		last_name = data.get('old_last_name')
+
 		user = None
-		# import pdb; pdb.set_trace()
 		try:
 			user = CustomUser.objects.get(first_name__icontains=first_name,last_name__icontains=last_name)
 		except:
 			return JsonResponse({"failed":"Wrong combination of first name and last name"})
 		
+		#Check whether the creator of the user is trying to modify or not. If not then raise error.		
 		if request.user.id != user.bank_user.creator.id:
 			return JsonResponse({"failed":"You dont have the permission to delete this user."})
 
+		'''this list will keep a track of the fields getting updated so that only those fields need to be saved 
+		in the database'''
 		updated_fields=[]
 
 		if "first_name" in data:
@@ -133,6 +150,8 @@ def updateBankingUser(request):
 			updated_fields.append('last_name')
 		user.save(update_fields=updated_fields)
 
+		'''this list will keep a track of the fields getting updated so that only those fields need to be saved 
+		in the database'''
 		updated_fields=[]
 		iban = user.bank_user.iban
 		if 'countrycode' in data:
@@ -152,6 +171,7 @@ def updateBankingUser(request):
 		return JsonResponse({"success":True})
 
 	if request.method=="GET":
+		#this code will execute when the user has given us the first name and last name and needs other details.
 		if "first_name" in request.GET and "last_name" in request.GET:
 			first_name = request.GET.get('first_name')
 			last_name = request.GET.get('last_name')
@@ -161,6 +181,8 @@ def updateBankingUser(request):
 			except:
 				return HttpResponseRedirect(reverse('bank_users:updateUser')+"?update=0")
 			return render(request,'updateUser.html',{'userdetails':user,'iban':user.bank_user.iban})
+		
+		#This code will execute when user needs to enter the first name and last name to get the record.
 		else:
 			if "update" in request.GET:
 				if request.GET.get("update")=="0":
@@ -169,9 +191,9 @@ def updateBankingUser(request):
 			userform = UserForm()
 			return render(request,'updateUser.html',{'form':userform})
 
+#Delete the user 
 def deleteBankingUser(request):
 	if request.method=="POST":
-		# import pdb; pdb.set_trace()
 		first_name = request.POST.get("first_name")
 		last_name = request.POST.get("last_name")
 		user = None
